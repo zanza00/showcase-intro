7f0d65ffc9262aa2cec7f4b5e2c24fbe184a1e7b
Simone Picciani <codefloe@simonepicciani.it>
2025-12-09 13:13:19 +0100

    add headers to enable override

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -12,7 +14,21 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     devServer: {
-      port: 8083,
+      port: 8082,
+      server: {
+        // type: "https",
+        // options: {
+        //   key: fs.readFileSync(path.resolve(__dirname, "../.certs/localhost+2-key.pem")),
+        //   cert: fs.readFileSync(path.resolve(__dirname, "../.certs/localhost+2.pem")),
+        // },
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "X-Requested-With, content-type, Authorization",
+      },
     },
   });
 };
